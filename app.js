e9f65244c5bba602da2c9d3476e026367d578899
Emil Cardell <emil.cardell@gmail.com>
2016-01-30 12:02:42 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = React.createClass({
+    getInitialState() { return { counter: 0 }; },
+    resetCounter() { this.setState({ counter: 0 }); },
+    componentDidMount() {
+        this.setState({ counter: this.props.counterStart });
+        setInterval(() => {
+            this.setState({ counter: this.state.counter+1 });
+        }, 1000);
+    },
+    render() {
+        return (<div> <h1>{this.state.counter}</h1>
+          <button onClick={this.resetCounter}>Reset</button></div>);
+    }
+});
+
+export default App;
